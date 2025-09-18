443476b2b2c2df4068d8039c0d53dc74795b1d90
Alan Marcel <alanmarcel2@gmail.com>
2025-09-18 19:20:40 -0300

    Create p01_frutas.py
    Projeto frutas com árvore de decisão de classificação

---
+++ p01_frutas.py
@@ -0,0 +1,31 @@
+# Importação dos dados:
+import pandas as pd
+
+df = pd.read_excel("dados_frutas.xlsx")
+df
+
+# Separação de dados de entrada (X) e de saída (y) do modelo:
+y = df['Fruta']
+caracteristicas = ["Arredondada","Suculenta",'Vermelha','Doce']
+X = df[caracteristicas]
+
+# Criação e treino do modelo:
+from sklearn import tree
+arvore = tree.DecisionTreeClassifier(random_state=42)
+arvore.fit(X, y)
+
+# Uso do modelo para fazer predição:
+arvore.predict([[0, 0, 0, 0]])
+
+# Exibição do desenho da árvore de decisão:
+import matplotlib.pyplot as plt
+plt.figure(dpi=400, figsize=[4,4])
+
+tree.plot_tree(arvore,
+               feature_names=caracteristicas,
+               class_names=arvore.classes_,
+               filled=True)
+
+# Exibição das probabilidades de predição de cada classe:
+proba = arvore.predict_proba([[1,1,1,1]])[0]
+pd.Series(proba, index=arvore.classes_)
